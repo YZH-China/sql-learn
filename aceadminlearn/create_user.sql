6b0ec1d927675b881d220b0ef8723d6be496f6bd
YZH-China <289081808@qq.com>
2017-01-20 11:20:55 +0800

    添加aceadminlearn数据库

---
+++ aceadminlearn/create_user.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS user
+(
+	id	int(11)		not null	auto_increment,
+	username	varchar(20)		not null,
+	password	varchar(20)		not null,
+	email	varchar(50)		not null,
+	niconame	varchar(20)		null,
+	birthday	date 	null,
+	reg_date	date 	not null,
+	gender	int(4)	null,
+	comment		varchar(255)	null,
+	website		varchar(50)		null,
+	phone		varchar(20)		null,
+	facebook	varchar(50)		null,
+	twitter		varchar(50)		null,
+	google		varchar(50)		null,
+	primary key (id)
+)engine=innoDB default charset=utf8;
